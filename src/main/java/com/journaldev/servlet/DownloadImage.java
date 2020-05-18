74317a8895089956a8bdb7b6765e8350bfd8fd77
ayyappa <ayyappap123@gmail.com>
2020-05-18 21:46:15 +0530

    added files

---
+++ src/main/java/com/journaldev/servlet/DownloadImage.java
@@ -0,0 +1,143 @@
+package com.journaldev.servlet;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics2D;
+import java.awt.Point;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.URL;
+import java.util.UUID;
+
+import javax.imageio.ImageIO;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+
+@WebServlet("/DownloadImage")
+public class DownloadImage extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    private ServletFileUpload uploader = null;
+	@Override
+	public void init() throws ServletException{
+		DiskFileItemFactory fileFactory = new DiskFileItemFactory();
+		File filesDir = (File) getServletContext().getAttribute("FILES_DIR_FILE");
+		fileFactory.setRepository(filesDir);
+		this.uploader = new ServletFileUpload(fileFactory);
+	}
+	/*protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		response.setCharacterEncoding("UTF-8");
+		String fileName = request.getParameter("fileName");
+		if(fileName == null || fileName.equals("")){
+			throw new ServletException("File Name can't be null or empty");
+		}
+		File file = new File(request.getServletContext().getAttribute("FILES_DIR")+File.separator+fileName);
+		if(!file.exists()){
+			throw new ServletException("File doesn't exists on server.");
+		}
+		System.out.println("File location on server::"+file.getAbsolutePath());
+		ServletContext ctx = getServletContext();
+		InputStream fis = new FileInputStream(file);
+		String mimeType = ctx.getMimeType(file.getAbsolutePath());
+		response.setContentType(mimeType != null? mimeType:"application/octet-stream");
+		response.setContentLength((int) file.length());
+		response.setHeader("Content-Disposition", "attachment; filename=\"" + fileName + "\"");
+		
+		ServletOutputStream os       = response.getOutputStream();
+		byte[] bufferData = new byte[1024];
+		int read=0;
+		while((read = fis.read(bufferData))!= -1){
+			os.write(bufferData, 0, read);
+		}
+		os.flush();
+		os.close();
+		fis.close();
+		System.out.println("File downloaded at client successfully");
+	}*/
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		response.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html");
+//		PrintWriter out = response.getWriter();
+//		out.write("<html><head></head><body>");
+		try {
+			String recipient = request.getParameter("recipient");
+			String dropDown = request.getParameter("dropdown");
+			
+//			String url = request.getServletContext().getAttribute("FILES_DIR")+"\\eid.jpg";
+			String url = getServletContext().getRealPath("img//eid.jpg");
+	        byte[] b = mergeImageAndText(url, dropDown +"/"+recipient, new Point(1000,1000));
+	        File file = new File(request.getServletContext().getAttribute("FILES_DIR")+File.separator+"SAVC_Greeting"+UUID.randomUUID()+".jpeg");
+	        FileOutputStream fos = new FileOutputStream(file);
+	       /* out.write("<div id=\"download\">File "+file.getName()+ " uploaded successfully.");*/
+//	        out.write("<br><br>");
+//			out.write("<a href=\"DownloadImage?fileName="+file.getName()+"\">تحميل </a>");
+	        fos.write(b);
+	        fos.close();
+	        downloadFile(request, response, file.getName());
+//	        request.getRequestDispatcher("EmailForm.jsp").include(request, response);
+			
+		} catch (Exception e) {
+//			out.write("Exception in uploading file.");
+		}
+//		out.write("</div></body></html>");
+	}
+    private void downloadFile(HttpServletRequest request,HttpServletResponse response,String fileName) throws ServletException, IOException {
+    	request.setCharacterEncoding("UTF-8");
+		response.setCharacterEncoding("UTF-8");
+		if(fileName == null || fileName.equals("")){
+			throw new ServletException("File Name can't be null or empty");
+		}
+		File file = new File(request.getServletContext().getAttribute("FILES_DIR")+File.separator+fileName);
+		if(!file.exists()){
+			throw new ServletException("File doesn't exists on server.");
+		}
+		System.out.println("File location on server::"+file.getAbsolutePath());
+		ServletContext ctx = getServletContext();
+		InputStream fis = new FileInputStream(file);
+		String mimeType = ctx.getMimeType(file.getAbsolutePath());
+		response.setContentType(mimeType != null? mimeType:"application/octet-stream");
+		response.setContentLength((int) file.length());
+		response.setHeader("Content-Disposition", "attachment; filename=\"" + fileName + "\"");
+		
+		ServletOutputStream os  = response.getOutputStream();
+		byte[] bufferData = new byte[1024];
+		int read=0;
+		while((read = fis.read(bufferData))!= -1){
+			os.write(bufferData, 0, read);
+		}
+		os.flush();
+		os.close();
+		fis.close();
+		System.out.println("File downloaded at client successfully");
+		
+	}
+	public static byte[] mergeImageAndText(String imageFilePath,
+            String text, Point textPosition) throws IOException {
+        BufferedImage im = ImageIO.read(new File(imageFilePath));
+        Graphics2D g2 = im.createGraphics();
+        g2.setColor(new Color (46, 59, 111));
+        g2.setFont(new Font("TimesRoman", Font.PLAIN, 50)); 
+        g2.drawString(text, 200 , 750);
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ImageIO.write(im, "jpeg", baos);
+        return baos.toByteArray();
+    }
+}
---
+++ src/main/webapp/EmailForm.jsp
@@ -0,0 +1,67 @@
+<%@page language="java" contentType="text/html; charset=UTF-8"%>
+<!DOCTYPE html>
+<html lang="ar" dir="rtl">
+<head>
+    <meta charset="utf-8">
+<title>Greetings</title>
+<style>
+.center {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+body{ margin:0 auto; }
+div {
+    height: 200px;
+    width: 400px;
+  
+
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    margin-top: -100px;
+    margin-left: -200px;
+}
+
+</style>
+<script type="text/javascript">
+function hideShow(){
+	document.getElementById("download").style.display = "none";
+}
+</script>
+</head>
+<body onload="hideShow()">
+<div class="content">
+	<form action="DownloadImage" method="post">
+	<img src="./img/Logo.png" alt="SAVC" class="center">
+		<table border="0" align="center">
+			<caption><h2>عيد مبارك</h2></caption>
+			<tr>
+				<td><bdi>الصفه</bdi></td>
+				<td>
+					<select id="dropdown" name="dropdown" required>
+					<option value="">select</option>
+					
+						<option value="اخوكم
+">اخوكم</option>
+						<option value="اختكم
+">اختكم</option>				
+					</select>
+				</td>
+			</tr>
+			<tr>
+				<td dir="rtl" >الاسم</td>
+				<td><input type="text" name="recipient" size="50" required/></td>
+			</tr>
+			<tr>
+				<td colspan="2" align="center"><input type="submit" value="تحميل" /></td>
+			</tr>
+			
+		</table>
+		
+		
+	</form>
+	</div>
+</body>
+</html>
